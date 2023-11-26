7c5bf8798004a0fd42d284c6ee6c59d3dfa41694
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-26 11:17:30 +0000

    Api key added and starter coding in script.js file

---
+++ script.js
@@ -0,0 +1,13 @@
+let myapiKey = "29c1a7e7a33d36b071d80aa75f16b7bb0";
+
+let history = [];
+
+let formEl = $("#search-form");
+
+let inputEl = $("#search-input");
+
+let todayEl = $("#today");
+
+let forcastEl = $("#forcast");
+
+let historyEl = $("#history");

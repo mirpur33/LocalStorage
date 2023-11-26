c9fc98005de713a9c37e206d54ec0926c67aec65
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-26 12:09:04 +0000

    Fix coding while getting latitude and londitute

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myapiKey = "29c1a7e7a33d36b071d80aa75f16b7bb0";
+let myapiKey = "92aa4fc0f53c0e96e588c41ac3e76efb";
 
 let history = [];
 
@@ -13,59 +13,52 @@ let forcastEl = $("#forcast");
 let historyEl = $("#history");
 
 function initHistory() {
-    let previousHistory = localStorage.getItem("history");
-    if (previousHistory) {
-        history = JSON.parse(previousHistory);
-    }
-    displayHistory();
+  let previousHistory = localStorage.getItem("history");
+  if (previousHistory) {
+    history = JSON.parse(previousHistory);
+  }
+  displayHistory();
 }
 
 function displayHistory() {
-    //tbc
+  //tbc
 }
 
 function getWeather(data) {
-     //tbc
+  //tbc
 }
 
 function getLatLon(city) {
-    console.log ("city = ", city);
+  console.log("city = ", city);
 
-    let latLonUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myapiKey}`;
-    fetch(latLonUrl) 
-
-    .then (function (resp) {
-        return resp.json();
+  let latLonUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myapiKey}`;
+  fetch(latLonUrl)
+    .then(function (resp) {
+      return resp.json();
     })
     .then(function (data) {
-        if (!data[0]) {
-            alert("city not found");
-        } else {
-            console.log("data = ", data);
-            getWeather(data[0]);
-        }
+      console.log("data = ", data);
+      getWeather(data[0]);
     })
     .catch(function (err) {
-        console.error(err)
+      console.error(err);
     });
-
 }
 
 function searchWeatherhandler(evt) {
-    if (!inputEl.val()) {
-        return;
-    }
+  if (!inputEl.val()) {
+    return;
+  }
 
-    evt.preventDefault();
+  evt.preventDefault();
 
-    let city = inputEl.val().trim();
+  let city = inputEl.val().trim();
 
-    getLatLon(city);
+  getLatLon(city);
 
-    inputEl.val("");
+  inputEl.val("");
 }
 
 initHistory();
 
 formEl.on("submit", searchWeatherhandler);
-

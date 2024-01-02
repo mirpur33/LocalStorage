f0889625f2dd63305964680de451849499d414a6
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2024-01-02 11:52:46 +0000

    Final Java Script updated and all linked are working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let myapiKey = "92aa4fc0f53c0e96e588c41ac3e76efb";
+// let myapiKey = "92aa4fc0f53c0e96e588c41ac3e76efb"; ' New API key will be updated. Currently not working'
 
 // Get references to HTML elements using their IDs
 var searchInputEl = document.getElementById("search-input");
@@ -9,7 +9,8 @@ var cityHistoryEl = document.getElementById("city-history")
 // TUTOR SESSION: Tutor, suggested and use the new idea to handle the weather dashboard... 
 
 var indexs = [0,7,15,23,31,39];
-// Set up arrays of references to HTML elements
+
+// Set up arrays of references to HTML elements - Suggested by tutor
 var cityDayEl = [
   document.getElementById("cityday"),
   document.getElementById("cityday1"),
@@ -54,3 +55,156 @@ var weatherIconEls = [
   document.getElementById("weather-icon4"),
   document.getElementById("weather-icon5")
 ];
+
+// Full tutor session applied and taken the idea to create functions //
+
+// Get reference to HTML element where weather information will be displayed
+
+var weatherDisplayEl = document.getElementById("displayweather");
+var weatherIconEl = document.getElementById("weather-icon");
+
+// Set API key and initialize empty arrays for cities, city history, and weather data
+
+// New API key updated to check the status and functions //
+
+var apiKey = "29c1a7e7a33d36b071d80aa75f16b7bb";
+var cities=[];
+var city_history=[];
+var temps=[6];
+var humids=[6];
+var winds=[6];
+var dates=[6];
+
+// Function to get the latitude and longitude for a given city
+
+function getLocation(city) {
+  console.log("find");
+  var locationUrl =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+
+  // Fetch data from the API and return an object
+  
+  fetch(locationUrl)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data[0]);
+      console.log(data[0].name);
+    var lat = data [0].lat;
+    var lon =data [0].lon;
+    getWeather(lat, lon)
+    });
+}
+
+// Function to get the weather data for a given latitude and longitude
+
+function getWeather(lat, lon){
+  var weatherUrl =
+  "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=metric"+"&lang=english"+"&appid="+apiKey;
+fetch(weatherUrl)
+  .then(function (data) {
+    return data.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    displayWeather(data);
+  });
+}
+
+// Here the function takes in weather data from an API and updates the relevant DOM elements with the weather information
+
+function displayWeather(data) {
+console.log(data);
+
+// Extract the relevant weather data from the API response
+
+var city = data.city.name;
+cityHistoryEl.textcontext= "";
+
+// Remember, pleaase loop through the first 6 items in the API response and extract the temperature, humidity, wind speed, and date
+
+for (let i=0;i<6;i++){
+  temps[i] = data.list[indexs[i]].main.temp;
+  humids[i] = data.list[indexs[i]].main.humidity;
+  winds[i] = data.list[indexs[i]].wind.speed;
+  dates[i] = new Date(data.list[indexs[i]].dt * 1000);
+}
+
+// Add the first 5 items in the API response to the "cities" array
+
+for (let i=0;i<5;i++){
+  cities.push(data.list[i]);
+}
+
+ // Update the DOM elements with the relevant weather data
+
+for (let j=0; j<6; j++){
+  tempEls[j].textContent = "Temperature: " + temps[j] + "°C";
+  humidEls[j].textContent = "Humidity: " + humids[j] + "%";
+  windEls[j].textContent = "Wind Speed: " + winds[j] + "KM/H";
+  cityDayEl[j].textContent = "City Day: " + city + " " + dates[j].toLocaleDateString()
+  weatherIconEls[j].setAttribute("src", `https://openweathermap.org/img/w/${data.list[j].weather[0].icon}.png`);
+}
+}
+
+// This event listener is triggered when the user clicks the search button. 
+
+searchButtonEl.addEventListener("click", function () {
+  var searchInput = searchInputEl.value; 
+  getLocation(searchInput);
+
+  // Add the user's search input to the "city_history" array and update the city history on the page
+  
+  if (city_history.length<3){
+  console.log(city_history);
+  city_history.unshift(searchInput);
+  console.log(city_history);
+  } else{
+    city_history.length= city_history.length-1;
+    city_history.unshift(searchInput);
+  }
+  printCityHistory();
+  updateCityHistory(city_history);
+});
+
+// Retrieve city history from local storage //
+
+var city_history = JSON.parse(localStorage.getItem("city_history")) || [];
+
+// This function prints the city history on the page, with each city as a button that can be clicked to retrieve the weather data
+
+function printCityHistory() {
+  cityHistoryEl.innerHTML = "";
+  console.log(cityHistoryEl);
+  for (let i = 0; i < city_history.length; i++) {
+    const list = document.createElement("li");
+    list.setAttribute("id",city_history[i]);
+    cityHistoryEl.appendChild(list);
+    const container = document.getElementById(city_history[i]);
+    const button = document.createElement("button");
+    button.setAttribute("value",city_history[i]);
+    button.textContent = city_history[i];
+    container.appendChild(button);
+    button.addEventListener("click", function(event){
+    const city = event.target.value;
+    console.log(city);
+    getLocation(city);
+    })
+  }
+}
+// This function updates the city history in local storage with the user's search input and calls the "printCityHistory" function to update the city history on the page
+
+function updateCityHistory(searchInput) {
+  localStorage.setItem("city_history", JSON.stringify(searchInput));
+  printCityHistory();
+}
+// Load city history on page load
+
+printCityHistory();
+
+// Thank God. It is finally working. Thank you. //

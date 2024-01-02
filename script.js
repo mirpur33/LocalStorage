efbc057aedf512b312fd19be5ddc21e4ea766344
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2024-01-02 09:59:18 +0000

    Tutor session used to upade the Java and new idea approached

--- script.js
+++ script.js
@@ -1,182 +1,56 @@
-let myapiKey = "92aa4fc0f53c0e96e588c41ac3e76efb";
+// let myapiKey = "92aa4fc0f53c0e96e588c41ac3e76efb";
 
-let history = [];
+// Get references to HTML elements using their IDs
+var searchInputEl = document.getElementById("search-input");
+var searchButtonEl = document.getElementById("search-button");
+var cityNameEl = document.getElementById("cityname");
+var cityHistoryEl = document.getElementById("city-history")
 
-let formEl = $("#search-form");
+// TUTOR SESSION: Tutor, suggested and use the new idea to handle the weather dashboard... 
 
-let inputEl = $("#search-input");
+var indexs = [0,7,15,23,31,39];
+// Set up arrays of references to HTML elements
+var cityDayEl = [
+  document.getElementById("cityday"),
+  document.getElementById("cityday1"),
+  document.getElementById("cityday2"),
+  document.getElementById("cityday3"),
+  document.getElementById("cityday4"),
+  document.getElementById("cityday5"),
+];
 
-let todayEl = $("#today");
+var tempEls = [
+  document.getElementById("temp"), 
+  document.getElementById("temp1"),
+  document.getElementById("temp2"),
+  document.getElementById("temp3"),
+  document.getElementById("temp4"),
+  document.getElementById("temp5"),
+];
 
-let forcastEl = $("#forecast");
+var humidEls = [
+  document.getElementById("humid"), 
+  document.getElementById("humid1"),
+  document.getElementById("humid2"),
+  document.getElementById("humid3"),
+  document.getElementById("humid4"),
+  document.getElementById("humid5")
+];
 
-let historyEl = $("#history");
+var windEls = [
+  document.getElementById("wind"), 
+  document.getElementById("wind1"),
+  document.getElementById("wind2"),
+  document.getElementById("wind3"),
+  document.getElementById("wind4"),
+  document.getElementById("wind5")
+];
 
-function initHistory() {
-  let previousHistory = localStorage.getItem("history");
-  if (previousHistory) {
-    history = JSON.parse(previousHistory);
-  }
-  displayHistory();
-}
-
-function displayHistory() {
-  //tbc
-}
-
-function displayForecastWeather(data) {
-  console.log(data);
-  // Write a function that filters the dt_text key to find the next 5 days
-  // Your algorithm should loop through the data object and filter for the next day
-  // 2023-12-13 || 2023-12-14 
-  //tbc
-  console.log("forecastEl = ", forcastEl);
-  // forcastEl.classList.remove("hide");
-  forcastEl[0].classList.remove("hide");
-
-  let headingElem = document.getElementById("date1");
-  let weatherIconElem = $('<img>');
-  let tempElem = document.getElementById("temp1");
-  let windElem = document.getElementById("wind1");
-  let humidityElem = document.getElementById("humidity1");
-
-  let date = data[0].dt_txt.split(" ")[0];
-  console.log("date = ", date);
-
-  let temp = data[0].main.temp;
-  let wind = data[0].wind.speed;
-  let humidity = data[0].main.humidity;
-
-  console.log("headingElem = ", headingElem);
-  headingElem.innerText=(date);
-  tempElem.innerText=`Temp: ${temp}`;
-  windElem.innerText=`Wind: ${wind}`;
-  humidityElem.innerText=`Humidity: ${humidity}`;
-
-  headingElem = document.getElementById("date2");
-  weatherIconElem = $('<img>');
-  tempElem = document.getElementById("temp2");
-  windElem = document.getElementById("wind2");
-  humidityElem = document.getElementById("humidity2");
-
-  date = data[0].dt_txt.split(" ")[0];
-  console.log("date = ", date);
-
-  temp = data[+4].main.temp;
-  wind = data[+4].wind.speed;
-  humidity = data[+4].main.humidity;
-
-  console.log("headingElem = ", headingElem);
-  headingElem.innerText=(date);
-  tempElem.innerText=`Temp: ${temp}`;
-  windElem.innerText=`Wind: ${wind}`;
-  humidityElem.innerText=`Humidity: ${humidity}`;
- }
-
- 
-function displayCurrentWeather(city, data) {
-  //tbc
-  let date = data.dt_txt.split(" ")[0];
-  console.log("date = ", date);
-
-  let temp = data.main.temp;
-  let wind = data.wind.speed;
-  let humidity = data.main.humidity;
-
-  console.log("temp = ", temp);
-  console.log("wind = ", wind);
-  console.log("humidity = ", humidity);
-
-  let icon =
-    "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-  let iconDescr = data.weather[0].description || data[0].main;
-
-  console.log("icon = ", icon);
-  console.log("iconDescr = ", iconDescr);
-
-  let cardElem = $('<div>');
-  let cardBodyElem = $('<div>');
-  let headingElem = $('<h2>');
-  let weatherIconElem = $('<img>');
-  let tempElem = $('<p>');
-  let windElem = $('<p>');
-  let humidityElem = $('<p>');
-
-  headingElem.text(city + ' (' + date + ')');
-  weatherIconElem.attr('src', icon);
-  weatherIconElem.attr('alt', iconDescr);
-  headingElem.append(weatherIconElem);
-
-  tempElem.text('Temp: ' + temp + ' °C');
-  windElem.text('Wind: ' + wind + ' KPH');
-  humidityElem.text('Humidity: ' + humidity + '%');
-  cardBodyElem.append(headingElem, tempElem, windElem, humidityElem);
-  cardElem.append(cardBodyElem);
-
-  todayEl.html('');
-  console.log("cardElem = ", cardElem);
-  todayEl.append(cardElem);
-
-}
-
-function displayWeather(city, data) {
-  //tbc
-  displayCurrentWeather(city, data.list[0]);
-  displayForecastWeather(data.list);
-}
-
-function getWeather(data) {
-  //tbc
-  let { lat } = data.coord;
-  let { lon } = data.coord;
-  let city = data.name;
-
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${myapiKey}&units=metric`;
-  fetch(weatherUrl)
-    .then(function (resp) {
-      return resp.json();
-    })
-    .then(function (data) {
-      console.log("data = ", data);
-      displayWeather(city, data);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-}
-
-function getLatLon(city) {
-  console.log("city = ", city);
-
-  let latLonUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myapiKey}`;
-  fetch(latLonUrl)
-    .then(function (resp) {
-      return resp.json();
-    })
-    .then(function (data) {
-      console.log("data = ", data);
-      console.log("data.coord = ", data.coord);
-      getWeather(data);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-}
-
-function searchWeatherhandler(evt) {
-  if (!inputEl.val()) {
-    return;
-  }
-
-  evt.preventDefault();
-
-  let city = inputEl.val().trim();
-
-  getLatLon(city);
-
-  inputEl.val("");
-}
-
-initHistory();
-
-formEl.on("submit", searchWeatherhandler);
+var weatherIconEls = [
+  document.getElementById("weather-icon"),
+  document.getElementById("weather-icon1"),
+  document.getElementById("weather-icon2"),
+  document.getElementById("weather-icon3"),
+  document.getElementById("weather-icon4"),
+  document.getElementById("weather-icon5")
+];
